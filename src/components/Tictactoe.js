3ca0dad7e2d7bbb8a5daa0a91b0920c813150f6f
Faye Kelmith Rernyuy <fayekelmith@gmail.com>
2023-09-09 22:06:32 +0530

    getting there

--- src/components/Tictactoe.js
+++ src/components/Tictactoe.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 const Tictactoe = () => {
   return (
     <div>
@@ -14,29 +14,42 @@ const Tictactoe = () => {
 
 //for the main board now
 const Board = () => {
+  //we need to create this state to manage all 9 boxes. we cannot manage them locally.
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const handleClick = (i) => {
+    //to create a copy of the squares array:
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  };
   return (
+    //using props we will now communicate between the super component board and the sub components box.
     <div className="board">
-      <Line />
-      <Line />
-      <Line />
-    </div>
-  );
-};
-//for each row:
-const Line = () => {
-  return (
-    <div className="line">
-      <Box status="X" />
-      <Box />
-      <Box />
+      <div className="line">
+        <Box value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Box value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Box value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="line">
+        <Box value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Box value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Box value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="line">
+        <Box value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Box value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Box value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
     </div>
   );
 };
 
-//defining props
-//for each box:
-const Box = ({ status }) => {
-  return <p className="box">{status}</p>;
+const Box = ({ value, onSquareClick }) => {
+  return (
+    <p className="box" onClick={onSquareClick}>
+      {value}
+    </p>
+  );
 };
 
 //for the player buttons;
